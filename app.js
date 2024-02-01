1797dd017f9e73398cbe4f79cfd73288a9613dcb
Hasan-MydeenWeb <asanweb7799@gmail.com>
2024-02-01 17:02:56 +0530

    add headphone-multypage master

---
+++ app.js
@@ -0,0 +1,12 @@
+const icon = document.querySelector(".icon");
+const ul = document.querySelector("ul");
+
+icon.addEventListener("click", () => {
+    ul.classList.toggle('showData')
+    
+    if (ul.className === "showData") {
+        document.getElementById("bar").className = "fa-solid fa-xmark"
+    } else {
+        document.getElementById("bar").className = "fa-solid fa-bars"
+    }
+})
